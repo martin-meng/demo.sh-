519f7a5105cb166815788f5cdace89fbbf570b9c
夜未央 <martin-meng@outlook.com>
2018-10-03 02:21:59 +0800

    demo.sh的习题

--- demo.sh
+++ demo.sh
@@ -7,7 +7,7 @@ else
   mkdir css js
   touch index.html css/style.css js/main.js
   
-  echo -e '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' > index.html
+  echo -e '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' > index.html //单引号多用于字符串及代码片段，换行用\n且紧贴后面内容不用空格
   echo -e 'h1{color: red;}' > css/style.css
   echo -e 'var string = "Hello World"\nalert(string)' > js/main.js
 
